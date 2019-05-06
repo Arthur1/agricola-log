a71822b7e06bec3995336d7cd957ee840fd8f981
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2019-05-06 17:26:53 +0900

    ogp

--- fuel/app/views/template/head.php
+++ fuel/app/views/template/head.php
@@ -7,25 +7,16 @@
 	<?= Html::meta('description', $description ?? 'LogRicolaは、ツイッタラーのためのアグリコラ戦績管理サイトです。アグリコラの戦績を保存し、サマリー画像をTwitterに投稿できます。'); ?>
 	<meta name="author" content="Arthur">
 	<!-- OGP [ -->
-	<!--
 	<meta property="og:title" content="<?= $title ?? ''; ?> | LogRicola">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="<?= Uri::current(); ?>">
+	<meta property="og:url" content="<?= Uri::base(); ?>">
 	<meta property="og:site_name" content="LogRicola">
 	<meta property="og:description" content="<?= $description ?? 'LogRicolaは、ツイッタラーのためのアグリコラ戦績管理サイトです。アグリコラの戦績を保存し、サマリー画像をTwitterに投稿できます。'; ?>">
-	<?php if (isset($ogp_image_large)): ?>
-	<meta property="twitter:card" content="summary_large_image">
-	<meta property="og:image" content="<?= ''; //Asset::get_file($ogp_image_large, 'img'); ?>">
-	<?php elseif (isset($ogp_image)): ?>
 	<meta property="twitter:card" content="summary">
-	<meta property="og:image" content="<?= ''; //Asset::get_file($ogp_image, 'img'); ?>">
-	<?php else: ?>
-	<meta property="twitter:card" content="summary">
-	<meta property="og:image" content="<?= ''; //Uri::create('/apple-touch-icon.png'); ?>">
-	<?php endif; ?>
-	-->
+	<meta property="og:image" content="<?= Uri::create('/apple-touch-icon.png'); ?>">
+
 	<!-- OGP ] -->
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="manifest" href="/manifest.json">
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP">
